0e2f53bf118713ff198fe7b5cd4479a3368c13b2
Itzik Naim <Itzik@cyberhat.co.il>
2018-04-04 11:57:09 +0300

    Blablabla

--- example/app.py
+++ example/app.py
@@ -48,6 +48,7 @@ class TestView(ResourceView):
     resource = TestResource
     methods = [Create, Update, Fetch, List, Delete]
 
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
